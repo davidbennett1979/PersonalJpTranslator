2b3c28e6d43d48dbeb1924e8ca72d9767f20b3df
David Bennett <davidbennett1979@gmail.com>
2025-11-14 19:28:25 -0600

    Initial Commit

---
+++ PersonalJpTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalJpTranslator
+//
+//  Created by David Bennett on 11/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalJpTranslator/PersonalJpTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalJpTranslatorApp.swift
+//  PersonalJpTranslator
+//
+//  Created by David Bennett on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalJpTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
